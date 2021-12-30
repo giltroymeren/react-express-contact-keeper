d2fe78094a9f90fc1f3bdb94deed2d0c258385ae
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2021-12-30 13:57:18 +0800

    Setup initial layout and filler pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import React from 'react';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 const App = () => {
   return (
-    <div>
-      <h1>Contact Keeper</h1>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <NavBar />
+
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 

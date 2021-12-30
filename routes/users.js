45a3adc8f0d1c2e05158b51e004f05525d9aac13
Giltroy P. Meren <giltroy.meren@GFI00245.local>
2021-12-30 10:29:57 +0800

    Setup POST user to db

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const { append } = require('express/lib/response')
 const router = express.Router()
 const { body, validationResult } = require('express-validator');
 
-const User = require('../models/User')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs/dist/bcrypt');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -15,14 +16,37 @@ router.post('/',
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Enter a password with 6 or more charatcers').isLength({ min: 6 }),
   ],
-  (request, response) => {
+  async (request, response) => {
     const errors = validationResult(request)
 
     if (!errors.isEmpty()) {
       return response.status(400).json({ errors: errors.array() })
     }
     
-    response.send('Register a user')
+    const { name, email, password } = request.body
+    try {
+      let user = await User.findOne({ email })
+
+      if(user) {
+        return response.status(400).json({ msg: 'Email already taken' })
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      })
+
+      const salt = await bcrypt.genSalt(15)
+      user.password = await bcrypt.hash(password, salt)
+
+      await user.save()
+
+      response.send(`${email} registered!`)
+    } catch (error) {
+      console.error(error.message)
+      return response.status(500).send('Server error occurred')
+    }
   })
 
 module.exports = router
